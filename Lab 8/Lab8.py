d4fafd9feabef91ca3eee6646ea8fca6bca175ae
JenniferOji <g00413791@atu.ie>
2024-11-19 10:50:00 +0000

    created a 2 x 2 subplot

--- Lab 8/Lab8.py
+++ Lab 8/Lab8.py
@@ -9,23 +9,24 @@ img = cv2.imread('ATU.jpg',)
 # Convert the image to grayscale
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
 # plot a line, implicitly creating a subplot(111)
 plt.plot([1,2,3])
 # now create a subplot which represents the top plot of a grid
 # with 2 rows and 1 column. Since this subplot will overlap the
 # first, the plot (and its axes) previously created, will be removed
-plt.subplot(211)
-plt.plot(range(12))
-plt.subplot(212, facecolor='y') # creates 2nd subplot with yellow background
+# plt.subplot(221)
+# plt.plot(range(12))
+# plt.subplot(212, facecolor='y') # creates 2nd subplot with yellow background
 
-plt.subplot(2, 1,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cmap = 'gray')
+# to control the amount of rows and columns 
+nrows = 2
+ncols = 2
+
+plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 
-
-plt.subplot(2, 1,2),plt.imshow(gray_image, cmap = 'gray')
+plt.subplot(nrows, ncols,2),plt.imshow(gray_image, cmap = 'gray')
 plt.title('Gray Scale'), plt.xticks([]), plt.yticks([])
 plt.show()
-
 
 
