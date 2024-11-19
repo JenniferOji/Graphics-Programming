2336f45e117d847fcf623f47ae0e6d2422a5ca6e
JenniferOji <g00413791@atu.ie>
2024-11-19 10:31:44 +0000

    subplot added to hold two images

--- Lab 8/Lab8.py
+++ Lab 8/Lab8.py
@@ -1,13 +1,31 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+
 
 img = cv2.imread('ATU.jpg',)
 
 # Convert the image to grayscale
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Converting image to grayscale 
-cv2.imshow('Grayscale Image', gray_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows() 
+
+# plot a line, implicitly creating a subplot(111)
+plt.plot([1,2,3])
+# now create a subplot which represents the top plot of a grid
+# with 2 rows and 1 column. Since this subplot will overlap the
+# first, the plot (and its axes) previously created, will be removed
+plt.subplot(211)
+plt.plot(range(12))
+plt.subplot(212, facecolor='y') # creates 2nd subplot with yellow background
+
+plt.subplot(2, 1,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+
+
+plt.subplot(2, 1,2),plt.imshow(gray_image, cmap = 'gray')
+plt.title('Gray Scale'), plt.xticks([]), plt.yticks([])
+plt.show()
+
+
+
