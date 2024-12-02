ced1da9e8bc5ff853a5e762558c5f3e2abd67743
JENNIFER OJI <G00413791@atu.ie>
2024-12-02 23:07:32 +0000

    plotted using the Shi Tomasi algorithm

--- Lab 9/Lab9.py
+++ Lab 9/Lab9.py
@@ -8,9 +8,13 @@ import copy
 # img = cv2.imread('ATU.jpg',)
 img = cv2.imread('ATU1.jpg',)
 
+# creating a copy of the orignal image so it doesnt affect the orignal 
+imgHarris = copy.deepcopy(img)
+
 # Convert the image to grayscale
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-
+# creating a copy of the gray image so it doesnt affect the orignal 
+imgShiTomasi = copy.deepcopy(gray_image)
 # to control the amount of rows and columns 
 nrows = 2
 ncols = 3
@@ -19,8 +23,6 @@ ncols = 3
 plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 
-# creating a copy of the orignal image so it doesnt affect the orignal 
-imgHarris = copy.deepcopy(img)
 # gray scale image
 plt.subplot(nrows, ncols,2),plt.imshow(cv2.cvtColor(gray_image, cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title('Gray Image'), plt.xticks([]), plt.yticks([])
@@ -37,5 +39,18 @@ for i in range(len(dst)):
 
 plt.subplot(nrows, ncols,3),plt.imshow(cv2.cvtColor(imgHarris, cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title('Harris Corner Detection'), plt.xticks([]), plt.yticks([])
+
+# shi tomasi algorithm
+# how corners are detected 
+# 0.01 = quality level - 10 = min distance - 30 = max corners 
+corners = cv2.goodFeaturesToTrack(gray_image,150,0.01,10)
+corners = np.int0(corners) #convert corners values to integer
+
+for i in corners:
+    x,y = i.ravel()
+    cv2.circle(imgShiTomasi,(x,y),3,(0, 255, 0),-1)
+
+plt.subplot(nrows, ncols,4),plt.imshow(cv2.cvtColor(imgShiTomasi, cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.title('Shi Tomasi algorithm'), plt.xticks([]), plt.yticks([])
 
 plt.show()
