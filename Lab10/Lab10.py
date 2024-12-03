dbaba758f595e9d88c9b317cdfe6bf78eec2af3d
JENNIFER OJI <G00413791@atu.ie>
2024-12-03 10:03:39 +0000

    multiple frame tracking

--- Lab10/Lab10.py
+++ Lab10/Lab10.py
@@ -1,4 +1,7 @@
+from collections import defaultdict
+
 import cv2
+import numpy as np
 
 from ultralytics import YOLO
 
@@ -9,6 +12,9 @@ model = YOLO("yolo11n.pt")
 video_path = "2103099-uhd_3840_2160_30fps.mp4"
 cap = cv2.VideoCapture(video_path)
 
+# Store the track history
+track_history = defaultdict(lambda: [])
+
 # Loop through the video frames
 while cap.isOpened():
     # Read a frame from the video
@@ -18,8 +24,24 @@ while cap.isOpened():
         # Run YOLO11 tracking on the frame, persisting tracks between frames
         results = model.track(frame, persist=True)
 
+        # Get the boxes and track IDs
+        boxes = results[0].boxes.xywh.cpu()
+        track_ids = results[0].boxes.id.int().cpu().tolist()
+
         # Visualize the results on the frame
         annotated_frame = results[0].plot()
+
+        # Plot the tracks
+        for box, track_id in zip(boxes, track_ids):
+            x, y, w, h = box
+            track = track_history[track_id]
+            track.append((float(x), float(y)))  # x, y center point
+            if len(track) > 30:  # retain 90 tracks for 90 frames
+                track.pop(0)
+
+            # Draw the tracking lines
+            points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+            cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
 
         # Display the annotated frame
         cv2.imshow("YOLO11 Tracking", annotated_frame)
